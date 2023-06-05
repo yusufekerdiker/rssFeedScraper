dc8b4e815198ea5b3733b679b95173365b2f3e85
Yusuf <ekerdiker.yusuf@gmail.com>
2023-06-05 09:57:43 +0300

    small cleanup

--- Backend/scraper.py
+++ Backend/scraper.py
@@ -10,8 +10,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 
-nltk.download('punkt')
-nltk.download('stopwords')
+nltk.download("punkt")
+nltk.download("stopwords")
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -21,7 +21,7 @@ load_dotenv(find_dotenv())
 password = os.environ.get("MONGODB_PWD")
 connection_string = f"mongodb+srv://yusufekerdiker:{password}@mycluster.fma9e4h.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(connection_string)
-database = client.rssFeeds4
+database = client.rssFeeds5
 
 rss_feed_links = {
     "CNET": "https://www.cnet.com/rss/news/",
@@ -29,6 +29,7 @@ rss_feed_links = {
 }
 
 existing_guids = set()
+
 
 def insertRssFeeds(db, rss_links):
     for feed_name, feed_url in rss_links.items():
@@ -73,38 +74,6 @@ def getArticleDescription(rss_url, item):
             return description.text
     else:
         return ""
-"""     
-    if rss_url == "https://www.cnet.com/rss/news/":
-        return item.description.text
-    elif rss_url == "https://www.wired.com/feed/rss":
-        return item.description.text 
-"""
-""" 
-        elif rss_url == "https://techspices.com/feed/":
-        desc_soup = BeautifulSoup(item.description.text, "html.parser")
-        first_p = desc_soup.find("p")
-        if first_p:
-            first_p_content = first_p.text
-        else:
-            first_p_content = ""
-        return first_p_content
-"""
-"""     
-    desc_soup = BeautifulSoup(desc, "html.parser")
-    if rss_url == "https://www.technopat.net/feed/":
-        first_p = desc_soup.find("p")
-        if first_p:
-            first_p_content = first_p.text
-        else:
-            first_p_content = ""
-        return first_p_content 
-
-    elif rss_url == "https://shiftdelete.net/feed":
-        last_p = desc_soup.find_all("p")[-1]
-        a_tag = last_p.find("a")
-        a_tag_content = a_tag.text if a_tag else ""
-        return a_tag_content
-"""
 
 
 def getArticleImage(rss_url, item):
@@ -116,15 +85,14 @@ def getArticleImage(rss_url, item):
         articleImage = item.thumbnail["url"]
         return articleImage
 
+
 def getArticleCategory(rss_url, item):
     category_elements = item.find_all("category")
     category_names = []
     for category_element in category_elements:
         category_names.append(category_element.text)
- 
+
     keywords_element = item.keywords
-    # keywords_list = keywords_element.text.split(", ") if keywords_element and keywords_element.text else []
-    
     keywords_list = []
     if keywords_element:
         keywords = keywords_element.text
@@ -137,19 +105,17 @@ def getArticleCategory(rss_url, item):
     desc_soup = BeautifulSoup(desc, "html.parser")
     description_text = desc_soup.get_text()
 
-    # Use RAKE to extract keywords
     r = Rake()
     r.extract_keywords_from_text(description_text)
 
-    # Get top 10 single-word and multi-word keywords
     ranked_phrases = r.get_ranked_phrases()
-    # keywords = r.get_ranked_phrases()[:10] # with this code rake only return top 10 popular multi-word or single word keywords which is sometimes not enough or not working properly so i modified to code for getting both type of keywords separately
+    # keywords = r.get_ranked_phrases()[:10] 
+    # with this code rake only return top 10 popular multi-word or single word keywords which is sometimes not enough or not working properly so i modified to code for getting both type of keywords separately
 
     single_words = []
     multi_words = []
-
     for phrase in ranked_phrases:
-        if ' ' in phrase:
+        if " " in phrase:
             multi_words.append(phrase)
         else:
             single_words.append(phrase)
@@ -157,32 +123,9 @@ def getArticleCategory(rss_url, item):
         if len(single_words) >= 5 and len(multi_words) >= 5:
             break
 
-    # Add keywords to existing categories
     categories = category_names + single_words[:5] + multi_words[:5] + keywords_list
 
     return categories
-
-"""
-#Extraction with nltk
-    desc_soup = BeautifulSoup(item.description.text, "html.parser")
-    description_text = desc_soup.get_text()
-
-    stop_words = set(stopwords.words('english'))
-
-    word_tokens = word_tokenize(description_text)
-
-    filtered_sentence = [w for w in word_tokens if not w in stop_words]
-
-    fdist = FreqDist(filtered_sentence)
-    most_common = fdist.most_common(5)  # adjust the number here to get more or less keywords
-
-    keywords = [word[0] for word in most_common]
-
-    # Add keywords to existing categories
-    categories = category_names + keywords
-
-    return categories
-"""
 
 
 if __name__ == "__main__":
